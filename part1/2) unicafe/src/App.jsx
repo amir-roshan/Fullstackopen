4c1e0292214e28bab01a72c4cc6bd9d3c7ceba79
amir-roshan <amirvalielahi@gmail.com>
2024-07-28 23:08:08 -0700

    1.8 - 1.11 : unicafe step 3 - 6

--- part1/2) unicafe/src/App.jsx
+++ part1/2) unicafe/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 
+const Statistics = ({ text, value }) => {
+    return <>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    </>;
+};
+
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0);
@@ -8,21 +17,36 @@ const App = () => {
 
     const getAllFeedbacks = () => good + neutral + bad;
     const getAverage = () => ((good + (bad * -1)) / (good + neutral + bad));
-    const getPositivePercentage = () => (good / (good + neutral + bad)) * 100;
+    const getPositivePercentage = () => (good / (good + neutral + bad)) * 100; 4;
 
-    return <>
-        <h1>Give Feedback</h1>
-        <button onClick={() => { setGood(good + 1); }}>Good</button>
-        <button onClick={() => { setNeutral(neutral + 1); }}>Nuetral</button>
-        <button onClick={() => { setBad(bad + 1); }}>Bad</button>
-        <h1>Statistics</h1>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {getAllFeedbacks()}</p>
-        <p>Average: {getAverage()}</p>
-        <p>Positive: {isNaN(getPositivePercentage()) ? 0 : getPositivePercentage()} %</p>
-    </>;
+    if (!(good || neutral || bad))
+        return <>
+            <h1>Give Feedback</h1>
+            <button onClick={() => { setGood(good + 1); }}>Good</button>
+            <button onClick={() => { setNeutral(neutral + 1); }}>Nuetral</button>
+            <button onClick={() => { setBad(bad + 1); }}>Bad</button>
+            <h2>No Feedback Given</h2>
+        </>;
+    else {
+        return <>
+            <h1>Give Feedback</h1>
+            <button onClick={() => { setGood(good + 1); }}>Good</button>
+            <button onClick={() => { setNeutral(neutral + 1); }}>Nuetral</button>
+            <button onClick={() => { setBad(bad + 1); }}>Bad</button>
+            <h1>Statistics</h1>
+            <table>
+                <tbody>
+
+                    < Statistics text={"Good"} value={good} />
+                    < Statistics text={"Neutral"} value={neutral} />
+                    < Statistics text={"Bad"} value={bad} />
+                    < Statistics text={"All"} value={getAllFeedbacks()} />
+                    < Statistics text={"Average"} value={isNaN(getAverage()) ? 0 : getAverage()} />
+                    < Statistics text={"Positive"} value={isNaN(getPositivePercentage()) ? 0 : getPositivePercentage() + "%"} />
+                </tbody>
+            </table>
+        </>;
+    }
 };
 
 export default App;
