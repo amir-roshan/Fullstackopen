635d2e74f0e3278d893143988c94ad41d3501879
amir-roshan <amirvalielahi@gmail.com>
2024-08-09 23:46:28 -0700

    2.18* Data for countries, step 1

--- part2/3) Data for countries/src/App.jsx
+++ part2/3) Data for countries/src/App.jsx
@@ -22,30 +22,37 @@ const App = () => {
     return (
         <>
             find countries <input type="text" onChange={handleSearch} />
-            {filteredCountries.length > 10
-                ? <p>Too many matches, specify another filter</p>
-                : filteredCountries.length === 1
-                    ? <div>
-                        <h1>{filteredCountries[0].name.common}</h1>
-                        <p>{filteredCountries[0].capital}</p>
-                        <p>{filteredCountries[0].area}</p>
-                        <h3>Languages</h3>
-                        <ul>
-                            {Object.keys(filteredCountries[0].languages).map(key => {
-                                <li key={key}>
-                                    {key}: {filteredCountries[0].languages[key]}
-                                </li>;
-                            })}
-                        </ul>
-                        <img src={filteredCountries[0].flags.png} alt={`${filteredCountries[0].name.common} flag`} />
-                    </div>
-                    : filteredCountries.length > 1
-                        ? <ul>
-                            {filteredCountries.map(country => (
-                                <li key={country.cca3}>{country.name.common}</li>
-                            ))}
-                        </ul>
-                        : <p>No matches found</p>
+            {filteredCountries.length > 10 && (
+                <p>Too many matches, specify another filter</p>
+            )}
+
+            {filteredCountries.length === 1 && (
+                <div>
+                    <h1>{filteredCountries[0].name.common}</h1>
+                    <p>Capital {filteredCountries[0].capital}</p>
+                    <p>Area {filteredCountries[0].area}</p>
+                    <h3>Languages</h3>
+                    <ul>
+                        {Object.entries(filteredCountries[0].languages).map(([key, value]) => (
+                            <li key={key}>
+                                {key}: {value}
+                            </li>
+                        ))}
+                    </ul>
+                    <img src={filteredCountries[0].flags.png} alt={`${filteredCountries[0].name.common} flag`} />
+                </div>
+            )}
+
+            {filteredCountries.length > 1 && filteredCountries.length <= 10 &&
+                <ul>
+                    {filteredCountries.map(country => (
+                        <li key={country.cca3}>{country.name.common}</li>
+                    ))}
+                </ul>
+            }
+
+            {filteredCountries.length === 0 &&
+                <p>No matches found</p>
             }
 
         </>
