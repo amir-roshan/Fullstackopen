9a96ba2f3eb07d4dd3712b84b92348f67c1d38fe
amir-roshan <amirvalielahi@gmail.com>
2024-08-10 10:23:27 -0700

    2.20*: Data for countries, step 3

--- part2/3) Data for countries/src/components/SingleCountry.jsx
+++ part2/3) Data for countries/src/components/SingleCountry.jsx
@@ -1,4 +1,40 @@
+import { useEffect, useState } from "react";
+import countryService from "../services/countries";
+
 const SingleCountry = ({ country }) => {
+    const [weather, setWeather] = useState({
+        temp: 0,
+        icon: '',
+        windSpeed: 0
+    });
+
+    const [loading, setLoading] = useState(true);
+
+    const celsiusConvertor = kelvin => (kelvin - 273.15).toFixed(2);
+
+    useEffect(() => {
+        const response = countryService.getWeather(country.capitalInfo.latlng[0], country.capitalInfo.latlng[1]);
+        response.then(res => {
+            const temp = celsiusConvertor(res.main.temp);
+
+            const weatherIcon = res.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+
+            const windSpeed = res.wind.speed;
+
+            setWeather({
+                temp: temp,
+                icon: iconUrl,
+                windSpeed: windSpeed
+            });
+
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+
+    console.log(weather);
     return (
         <>
             <h1>{country.name.common}</h1>
@@ -13,6 +49,10 @@ const SingleCountry = ({ country }) => {
                 ))}
             </ul>
             <img src={country.flags.png} alt={`${country.name.common} flag`} />
+            <h2>Weather in {country.capital}</h2>
+            <p>tempreture {weather.temp}°C</p>
+            <img src={weather.icon} alt="weather image" />
+            <p>wind {weather.windSpeed} m/s</p>
         </>
     );
 };
--- part2/3) Data for countries/src/services/countries.js
+++ part2/3) Data for countries/src/services/countries.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
-const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
+const contriesUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
 
 const getAllcountries = () => {
-    const res = axios.get(baseUrl);
+    const res = axios.get(contriesUrl);
     return res.then(respond => respond.data);
 };
 
-export default { getAllcountries };
+const getWeather = (lat, lon) => {
+    const res = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
+    return res.then(respond => respond.data);
+};
+
+export default { getAllcountries, getWeather };
