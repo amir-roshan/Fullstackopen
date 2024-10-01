9ff420890188cf09958ca655ea4e13f7918ec9b7
amir-roshan <amirvalielahi@gmail.com>
2024-10-01 15:57:13 -0700

    3.3: phonebook backend step3

--- part3/index.js
+++ part3/index.js
@@ -10,6 +10,18 @@ app.get("/api/persons", (req, res) => {
     res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person);
+    }
+    else {
+        res.status(404).send(`<h2>Person with the ID of ${id} is not found :(</h2>`);
+    }
+});
+
 app.get("/info", (req, res) => {
     let phoneBookLength = persons.length;
 
