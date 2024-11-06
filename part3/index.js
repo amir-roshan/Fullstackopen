061950c15af37ffd0ab8b0ad022985cc5cf3ac6e
amir-roshan <amirvalielahi@gmail.com>
2024-11-06 10:54:09 -0800

    3.4: phonebook backend step4

--- part3/index.js
+++ part3/index.js
@@ -7,27 +7,44 @@ const app = express();
 // app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons);
+  res.json(persons);
 });
 
 app.get("/api/persons/:id", (req, res) => {
-    const id = req.params.id;
-    const person = persons.find(person => person.id === id);
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        res.json(person);
-    }
-    else {
-        res.status(404).send(`<h2>Person with the ID of ${id} is not found :(</h2>`);
-    }
+  if (person) {
+    res.json(person);
+  } else {
+    res
+      .status(404)
+      .send(`<h2>Person with the ID of ${id} is not found :(</h2>`);
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const person = persons.find(
+    (person) => parseInt(person.id) === parseInt(req.params.id)
+  );
+
+  console.log(person);
+
+  if (!person) {
+    return res.status(404).send("<h2>Person is not found :(</h2>");
+  }
+
+  persons.splice(person.id - 1, 1);
+
+  res.send(person);
 });
 
 app.get("/info", (req, res) => {
-    let phoneBookLength = persons.length;
+  let phoneBookLength = persons.length;
 
-    let today = new Date();
+  let today = new Date();
 
-    res.send(`
+  res.send(`
         <h2>Phonebook has info for ${phoneBookLength} people</h2>
         <p>${today}</p>
         `);
