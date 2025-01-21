34ef12d6869fce278551d673ae26b9a3ec677bb6
amir-roshan <amirvalielahi@gmail.com>
2025-01-21 11:48:47 -0800

    Enhance security and update dependencies; add Helmet for CSP, modify README, and adjust Fly configuration

--- part3/README.md
+++ part3/README.md
@@ -1,1 +1,2 @@
-https://part3-little-lake-2511.fly.dev/api/persons
+Backend part => https://part3-little-lake-2511.fly.dev/api/persons
+Fullstack part => https://fullstackopen-zyj8.onrender.com/
--- part3/app.js
+++ part3/app.js
@@ -3,6 +3,7 @@ import { persons } from "./data.js";
 import { generateUniqueId } from "./utility.js";
 import morgan from "morgan";
 import cors from "cors";
+import helmet from "helmet";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -15,18 +16,26 @@ const logger = morgan(
   ":method :url :status :res[content-length] - :response-time ms :body"
 );
 
+app.use(
+  helmet({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: [
+        "'self'",
+        "https://part3-little-lake-2511.fly.dev",
+        "https://fonts.gstatic.com",
+      ],
+    },
+  })
+);
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(logger);
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; font-src 'self' data: https://part3-little-lake-2511.fly.dev; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self'; connect-src 'self'"
-  );
-  next();
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
 app.get("/api/persons", (req, res) => {
